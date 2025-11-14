0be64374a2a23b7708e2a2d96b492379798b38f0
Zahid Jamali <zahid.jamali4848@gmail.com>
2025-11-14 13:37:53 +0500

    index page is rendered

--- api/server.js
+++ api/server.js
@@ -5,7 +5,11 @@ const healthRoutes = require("./routes/healthRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 app.use("/", healthRoutes);
 
 console.log("server is running!");
